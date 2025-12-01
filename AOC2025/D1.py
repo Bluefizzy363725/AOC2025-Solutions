842bad47c6a1fed899e3f7dd267b1ed1be708ad9
olivi <olivi@OLIVIALAPTOP>
2025-12-01 17:21:20 +0000

    Add project files.

---
+++ AOC2025/D1.py
@@ -0,0 +1,29 @@
+with open('puzzleInput.txt') as f:
+    x = f.readlines()
+start = 50
+key = ''
+count = 0
+count2 = 0
+for i in x:
+    key = i[0]
+    match key:
+        case 'R':
+            start += int(i[1:])
+            while start > 99:
+                start -= 100
+                if start != 0:
+                    count += 1
+        case 'L':
+            if start == 0:
+                start += 100 - int(i[1:])
+            else:
+                start -= int(i[1:])
+            while start < 0:
+                start += 100
+                count += 1
+    if start == 0:
+        count += 1
+        count2 += 1
+
+print(count2)
+print(count)
